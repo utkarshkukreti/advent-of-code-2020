41f3c54848a0a69bfad17e20101646e4c8604638
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-12-02 12:57:22 +0530

    refactor 1.rb

--- 1.rb
+++ 1.rb
@@ -1,13 +1,13 @@
 require "set"
 
-xs = $<.read.split("\n").map(&:to_i).to_set
+set = $<.read.split("\n").map(&:to_i).to_set
 
-xs.any? do |a|
+set.any? do |a|
   b = 2020 - a
-  p a * b if xs.include?(b)
+  p a * b if set.include?(b)
 end
 
-xs.to_a.combination(2).any? do |a, b|
+set.to_a.combination(2).any? do |a, b|
   c = 2020 - a - b
-  p a * b * c if xs.include?(c)
+  p a * b * c if set.include?(c)
 end
