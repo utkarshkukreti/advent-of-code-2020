c7273dbbcfed075ca1dcabe8eedb74880bae3ca8
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-12-02 13:05:57 +0530

    refactor 2.rb

--- 2.rb
+++ 2.rb
@@ -1,7 +1,7 @@
 p1, p2 = 0, 0
 
 $<.read.strip.split("\n").each do |line|
-  _, a, b, c, d = line.match(/^(\d+)-(\d+) (.): (.*)/).to_a
+  _, a, b, c, d = line.match(/^(\d+)-(\d+) (.): (.+)$/).to_a
   a, b = a.to_i, b.to_i
   p1 += 1 if d.count(c) >= a && d.count(c) <= b
   p2 += 1 if (d[a - 1] == c) ^ (d[b - 1] == c)
