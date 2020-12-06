cb245e9882d592e0870bb5f2b556dd930e1bbb11
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-12-06 10:54:20 +0530

    refactor 6.rb

--- 6.rb
+++ 6.rb
@@ -1,11 +1,7 @@
-require "set"
-
 input = $<.read.split("\n\n")
 
 p input.map { |x| x.scan(/[a-z]/).uniq.count }.sum
 
 p(input.map do |x|
-  x.split("\n").map do |line|
-    line.chars.to_set
-  end.reduce(:&).count
+  x.split("\n").map(&:chars).reduce(:&).count
 end.sum)
