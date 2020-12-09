e729c3f499d7b8f94df454c063d636a461034293
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-12-09 11:19:09 +0530

    refactor 1.rb to be more functional

--- 1.rb
+++ 1.rb
@@ -2,12 +2,14 @@ require "set"
 
 set = $<.read.split("\n").map(&:to_i).to_set
 
-set.any? do |a|
+p1 = set.each do |a|
   b = 2020 - a
-  p a * b if set.include?(b)
+  break a * b if set.include?(b)
 end
 
-set.to_a.combination(2).any? do |a, b|
+p2 = set.to_a.combination(2).each do |a, b|
   c = 2020 - a - b
-  p a * b * c if set.include?(c)
+  break a * b * c if set.include?(c)
 end
+
+p p1, p2
