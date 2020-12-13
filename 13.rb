ba5dcbb9e2a023d42720c15f7c07edea5c7ceae0
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-12-13 14:40:41 +0530

    day 13

---
+++ 13.rb
@@ -0,0 +1,11 @@
+n = gets.to_i
+xs = gets.split(",").filter_map.with_index { |n, i| (n = n.to_i; [n, (n - i) % n]) if n != "x" }
+
+p xs.map { |x, _| [x, (0..).step(x).find { |y| y > n } - n] }.min_by(&:last).reduce(:*)
+
+f = ->a, b, c, d {
+  e = (b..).step(a).find { |x| x % c == d }
+  [a * c, e]
+}
+
+p xs.reduce { |(a, b), (c, d)| f.(a, b, c, d) }.last
